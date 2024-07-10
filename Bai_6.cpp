34e169f2743a4f84fcc849033bf3c10802294fbd
Hoang Khanh <khanhdo152003@gmail.com>
2024-07-10 16:50:21 +0700

    bai buoi1

---
+++ Bai_6.cpp
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main() {
+	char hoTen[100];
+	int ngaySinh, thangSinh, namSinh;
+	char gioiTinh;
+	setlocale(LC_ALL, "vi_VN.UTF-8");
+	printf("Nhap ho ten: ");
+	scanf_s("%s", hoTen);
+	printf("Nhap ngay thang nam sinh (dd mm yyyy): ");
+	scanf_s("%d %d %d", &ngaySinh, &thangSinh, &namSinh);
+	printf("Nhap gioi tinh (N hoac n cho nam, C hoac c cho nu): ");
+	scanf_s(" %c", &gioiTinh);
+
+	// Tính tuổi nghỉ hưu
+	int tuoiHuu;
+	if (gioiTinh == 'N' || gioiTinh == 'n') {
+		// Nam
+		tuoiHuu = 62;
+	}
+	else if (gioiTinh == 'C' || gioiTinh == 'c') {
+		// Nữ
+		tuoiHuu = 60;
+	}
+	else {
+		printf("Gioi tinh hop le.\n");
+		return 1;
+	}
+
+	int tuoiHienTai = 2024 - namSinh;
+	int thoiGianNghiHuu = tuoiHuu - tuoiHienTai;
+
+	if (thoiGianNghiHuu > 0) {
+		printf("Nguoi lao dong %s se duoc nghi huu %d nam.\n", hoTen, thoiGianNghiHuu);
+	}
+	else {
+		printf("Nguoi lao dong %s da du tuoi nghi huu.\n", hoTen);
+	}
+
+	system("pause");
+	return 0;
+}
---
+++ Baitap_5.cpp
@@ -0,0 +1,19 @@
+﻿//#include <stdio.h>
+//#include <conio.h>
+//#include <stdlib.h>
+//int main()
+//{ 
+//	int a, b, c;
+//	printf("Nhap vao so nguyen a =");
+//	scanf_s("%d",&a);
+//	printf("Nhap vao so nguyen b =");
+//	scanf_s("%d", &b);
+//	printf("Nhap vao so nguyen c =");
+//	scanf_s("%d", &c);
+//
+//	int Tong = (a - b) / c;
+//	printf("Ket qua = %d\n",Tong);
+//
+//	system("pause");
+//	return 0;
+//}
